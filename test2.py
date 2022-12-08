640de32845bed7e0bb24ce2f33e3f9da1d523b22
Ho Kwan Tang <51382053+htang4034@users.noreply.github.com>
2022-12-08 16:52:55 -0600

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,10 +2,12 @@ import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
+import ctypes
 
 # read before run
-print("WARNING, the program will keep on running until 'e' is pressed!")
-print("press 's' to start and pause.")
+# print("WARNING, the program will keep on running until 'e' is pressed!")
+# print("press 's' to start and pause.")
+print("running")
 delay = 0.000001
 button = Button.left
 start_stop_key = KeyCode(char='s')
@@ -42,18 +44,38 @@ click_thread = ClickMouse(delay, button)
 click_thread.start()
 
 
-def on_press(key):
-    if key == start_stop_key:
-        if click_thread.running:
-            click_thread.stop_clicking()
-            print("Program pause.")
-        else:
-            click_thread.start_clicking()
-    elif key == exit_key:
+# def on_press(key):
+#     if key == start_stop_key:
+#         if click_thread.running:
+#             # click_thread.stop_clicking()
+#             # ctypes.windll.user32.MessageBoxW(0, "Program paused. Press s to cont'd or press e to exit program", "Your title", 0x1000)
+#             # print("Program pause.")
+#             # changed here to press s to exit program too
+#             click_thread.exit()
+#             listener.stop()
+#             ctypes.windll.user32.MessageBoxW(0, "Program exit.", "Your title", 0x1000)
+#         else:
+#             click_thread.start_clicking()
+#     elif key == exit_key:
+#         click_thread.exit()
+#         listener.stop()
+#         print("Program EXIT.")
+#         ctypes.windll.user32.MessageBoxW(0, "Program exit.", "Your title", 0x1000)
+
+def on_press2(key):
+    if click_thread.running:
         click_thread.exit()
         listener.stop()
         print("Program EXIT.")
+        ctypes.windll.user32.MessageBoxW(0, "Program exit.", "Your title", 0x1000)
+    elif key == start_stop_key:
+        click_thread.start_clicking()
+    else:
+        ctypes.windll.user32.MessageBoxW(0, "Press s to start auto click", "Your title", 0x1000)
 
 
-with Listener(on_press=on_press) as listener:
+# with Listener(on_press=on_press) as listener:
+#     listener.join()
+
+with Listener(on_press=on_press2) as listener:
     listener.join()
